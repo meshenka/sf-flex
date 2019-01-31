cf948d0c21e736e9071aa2004ee6f0ac5938c729
Sylvain Gogel <sylvain.gogel@gmail.com>
2019-01-31 13:55:35 +0100

    fix(controller): use extends AbstractController

--- src/Http/Controller/IndexController.php
+++ src/Http/Controller/IndexController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/")
  */
-class IndexController extends Controller
+class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="app_index", methods="GET")
--- src/Http/Controller/ProductController.php
+++ src/Http/Controller/ProductController.php
@@ -5,8 +5,8 @@
  */
 namespace App\Http\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Domain\RequestCreator\ProductRequestCreator;
@@ -18,7 +18,7 @@ use App\Store\Entity\ProductEntity;
 /**
  * @Route("/")
  */
-class ProductController extends Controller
+class ProductController extends AbstractController
 {
     /**
      * @var ProductRequestCreator
