14c3b0db5af0970f819f4426749ed23f50b0e632
Sylvain Gogel <sylvain.gogel@gmail.com>
2019-02-07 12:47:05 +0100

    feat: add fixture loading

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $loader = new NativeLoader();
+        $objectSet = $loader->loadFile(__DIR__ . '/../../fixtures/10_product.yaml')->getObjects();
+    
+        foreach ($objectSet as $object) {
+            $manager->persist($object); 
+        }
+        
         $manager->flush();
     }
 }
+
--- src/Resources/Assets/js/app.js
+++ src/Resources/Assets/js/app.js
@@ -1,8 +1,9 @@
+// @flow
 /* eslint-disable no-unused-vars */
 import Css from '../stylesheets/app.scss'
 /* eslint-enable no-unused-vars */
 
 // a simple ES6 test
 export default {
-    config: true
+  config: true
 }
